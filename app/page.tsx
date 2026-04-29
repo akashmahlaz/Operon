99edaf2253910a17eb282a73fbd5aa72d973a932
akashmahlaz <akashdalla408@gmail.com>
2026-04-29 10:26:33 +0530

    built the header

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Message from '@/components/chat/Message';
 
 export default function Chat(){
  return(
-  <div className='bg-black min-h-screen w-full flex flex-col'>
+  <div className='min-h-screen w-full flex flex-col'>
    <Header/>
   </div>
  )
--- components/chat/Header.tsx
+++ components/chat/Header.tsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 
 export default function Header(){
     return(
-    <div className='w-full bg-gray-800 flex items-center justify-between px-4'>
-      <div className='flex px-2 py-1 bg-green-400 '>
-        <Image src={"/globe.svg"} alt='Logo' width={100} height={100} className="h-8 w-8 rounded-2xl bg-white"/>
+    <div className='w-full flex items-center justify-between px-4'>
+      <div className='flex px-2 py-1'>
+        <Image src={"/globe.svg"} alt='Logo' width={100} height={100} className="h-8 w-8 rounded-2xl "/>
       </div>
       <div className="flex px-2 py-1 justify-center items-center">
         <Button className="rounded-2xl ">Connect whatsapp<span className=""><ArrowRight className=""/></span></Button>
